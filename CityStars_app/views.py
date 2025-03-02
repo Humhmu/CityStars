d642c81a17d10c83bc837712704655e0239bfc84
Lewis Ng <2884302n@student.gla.ac.uk>
2025-03-02 13:50:19 +0000

    Added templates that extend from 'base.html' and amended 'CityStars_app/views.py'

--- CityStars_app/views.py
+++ CityStars_app/views.py
@@ -3,60 +3,60 @@ from django.shortcuts import render
 
 
 def city_stars(request):
-    return render(request, "CityStars_app/base.html")
+    return render(request, 'CityStars_app/city_stars.html')
 
 
 def city(request, city_slug):
-    return HttpResponse(f"City Page for {city_slug}")
+    return render(request, 'CityStars_app/city.html')
 
 
 def add_post(request, city_slug):
-    return HttpResponse(f"page to add post to {city_slug}")
+    return render(request, 'CityStars_app/add_post.html')
 
 
 def friend_feed(request):
-    return HttpResponse("Friend Feed Page")
+    return render(request, 'CityStars_app/friend_feed.html')
 
 
 def city_feed(request):
-    return HttpResponse("City Feed Page")
+    return render(request, 'CityStars_app/city_feed.html')
 
 
 def profile(request, profile_username):
-    return HttpResponse(f"{profile_username}'s profile page")
+    return render(request, 'CityStars_app/profile.html')
 
 
 def delete_profile(request, profile_username):
-    return HttpResponse(f"Page to delete {profile_username}'s profile")
+    return render(request, 'CityStars_app/delete_profile.html')
 
 
 def friends(request, profile_username):
-    return HttpResponse(f"{profile_username}'s friends page")
+    return render(request, 'CityStars_app/friends.html')
 
 
 def chat(request, profile_username, friend_username):
-    return HttpResponse(f"{profile_username}'s chat with {friend_username}")
+    return render(request, 'CityStars_app/chat.html')
 
 
 def posts(request, profile_username):
-    return HttpResponse(f"Posts page for {profile_username}")
+    return render(request, 'CityStars_app/posts.html')
 
 
 def city_post(request, city_slug, post_id):
-    return HttpResponse(f"Post {post_id} in {city_slug}")
+    return render(request, 'CityStars_app/city_post.html')
 
 
 def user_post(request, profile_username, post_id):
-    return HttpResponse(f"Post {post_id} from {profile_username}")
+    return render(request, 'CityStars_app/user_post.html')
 
 
 def delete_post(request, post_id):
-    return HttpResponse(f"Delete post {post_id}")
+    return render(request, 'CityStars_app/delete_post.html')
 
 
 def login(request):
-    return HttpResponse("Login Page")
+    return render(request, 'CityStars_app/login.html')
 
 
 def signup(request):
-    return HttpResponse("Signup Page")
+    return render(request, 'CityStars_app/register.html')
