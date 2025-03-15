6ed59cba9ba1255e9ed2e378d2dacabb074775e1
Joshua Niewczas <joshua.niewczas@glasgow.ac.uk>
2025-03-15 14:33:44 +0000

    added css + script

---
+++ static/js/add_post.js
@@ -0,0 +1,33 @@
+const titleInput = document.getElementById("title");
+const textInput = document.getElementById("text");
+const titleCharCount = document.getElementById("title-char-count");
+const textCharCount = document.getElementById("text-char-count");
+
+const imageInput = document.querySelector('input[type="file"]');
+const imagePreview = document.getElementById("image-preview");
+const imagePreviewContainer = document.getElementById(
+  "image-preview-container"
+);
+
+titleInput.addEventListener("input", () => {
+  const currentLength = titleInput.value.length;
+  titleCharCount.textContent = `${currentLength}/20`;
+});
+
+textInput.addEventListener("input", () => {
+  const currentLength = textInput.value.length;
+  textCharCount.textContent = `${currentLength}/500`;
+});
+
+imageInput.addEventListener("change", function (event) {
+  const file = event.target.files[0];
+
+  if (file && file.type.startsWith("image/")) {
+    const imageUrl = URL.createObjectURL(file);
+
+    imagePreview.src = imageUrl;
+    imagePreviewContainer.style.display = "block";
+  } else {
+    imagePreviewContainer.style.display = "none";
+  }
+});
