ac9607cc58b27ae3b3f96a2bbe7ae10145b2a3a1
Joshua Niewczas <joshua.niewczas@glasgow.ac.uk>
2025-03-15 21:32:06 +0000

    refinement to add_post page

--- static/js/add_post.js
+++ static/js/add_post.js
@@ -2,12 +2,12 @@ const titleInput = document.getElementById("title");
 const textInput = document.getElementById("text");
 const titleCharCount = document.getElementById("title-char-count");
 const textCharCount = document.getElementById("text-char-count");
+const alertList = document.querySelectorAll(".alert");
 
-const imageInput = document.querySelector('input[type="file"]');
-const imagePreview = document.getElementById("image-preview");
-const imagePreviewContainer = document.getElementById(
-  "image-preview-container"
-);
+document.addEventListener("DOMContentLoaded", function () {
+  titleCharCount.textContent = `${titleInput.value.length}/20`;
+  textCharCount.textContent = `${textInput.value.length}/500`;
+});
 
 titleInput.addEventListener("input", () => {
   const currentLength = titleInput.value.length;
@@ -19,15 +19,6 @@ textInput.addEventListener("input", () => {
   textCharCount.textContent = `${currentLength}/500`;
 });
 
-imageInput.addEventListener("change", function (event) {
-  const file = event.target.files[0];
-
-  if (file && file.type.startsWith("image/")) {
-    const imageUrl = URL.createObjectURL(file);
-
-    imagePreview.src = imageUrl;
-    imagePreviewContainer.style.display = "block";
-  } else {
-    imagePreviewContainer.style.display = "none";
-  }
+alertList.forEach(function (alert) {
+  new bootstrap.Alert(alert);
 });
