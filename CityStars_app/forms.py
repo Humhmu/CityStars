b9b9f82bfa6eddba72332abf056cf69a7cd35d1d
Joshua Niewczas <joshua.niewczas@glasgow.ac.uk>
2025-03-15 11:45:17 +0000

    add_post form and template added

--- CityStars_app/forms.py
+++ CityStars_app/forms.py
@@ -1,17 +1,25 @@
 from django import forms
 from django.contrib.auth.models import User
-from CityStars_app.models import Profile
+from CityStars_app.models import Profile, Post
+
 
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password',)
+        fields = ("username", "email", "password")
 
-        help_texts = {'username':''}
+        help_texts = {"username": ""}
+
 
 class UserProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ('profile_picture', 'bio', 'is_verified')
+        fields = ("profile_picture", "bio", "is_verified")
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ("title", "image", "text", "rating")
