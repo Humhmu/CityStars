1851994d0abe30d65531f549478bc708c3ff1f1a
Joshua Niewczas <joshua.niewczas@glasgow.ac.uk>
2025-03-17 12:42:34 +0000

    added image preview

--- static/js/add_post.js
+++ static/js/add_post.js
@@ -4,6 +4,12 @@ const titleCharCount = document.getElementById("title-char-count");
 const textCharCount = document.getElementById("text-char-count");
 const alertList = document.querySelectorAll(".alert");
 
+const imageInput = document.querySelector('input[type="file"]');
+const imagePreview = document.getElementById("image-preview");
+const imagePreviewContainer = document.getElementById(
+  "image-preview-container"
+);
+
 document.addEventListener("DOMContentLoaded", function () {
   titleCharCount.textContent = `${titleInput.value.length}/20`;
   textCharCount.textContent = `${textInput.value.length}/500`;
@@ -17,6 +23,19 @@ titleInput.addEventListener("input", () => {
 textInput.addEventListener("input", () => {
   const currentLength = textInput.value.length;
   textCharCount.textContent = `${currentLength}/500`;
+});
+
+imageInput.addEventListener("change", function (event) {
+  const file = event.target.files[0];
+
+  if (file && file.type.startsWith("image/")) {
+    const imageUrl = URL.createObjectURL(file);
+
+    imagePreview.src = imageUrl;
+    imagePreviewContainer.style.display = "block";
+  } else {
+    imagePreviewContainer.style.display = "none";
+  }
 });
 
 alertList.forEach(function (alert) {
