68b7b3b060a34a55ba357a823293948dc73e4f47
Joshua Niewczas <joshua.niewczas@glasgow.ac.uk>
2025-03-27 22:22:13 +0000

    added signal to update city rating and fixed css for post page

--- CityStars_app/signals.py
+++ CityStars_app/signals.py
@@ -1,13 +1,15 @@
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.dispatch import receiver
-from CityStars_app.models import Profile
+from CityStars_app.models import Profile, Post, City
+
 
 # creates a user profile for the user and connects profile to user
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
-        Profile.objects.create(user=instance) 
+        Profile.objects.create(user=instance)
+
 
 # saves profile when its been updated, if user doesnt have a profile creates one
 @receiver(post_save, sender=User)
@@ -16,3 +18,17 @@ def save_user_profile(sender, instance, **kwargs):
         instance.profile.save()
     except Profile.DoesNotExist:
         Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=Post)
+def update_city_rating(sender, instance, created, **kwargs):
+    if created:
+        city = City.objects.get(id=instance.city.id)
+        values = Post.objects.filter(city=city).values_list("rating", flat=True)
+        avg = 0
+        for v in values:
+            avg += int(v)
+
+        avg /= len(values)
+        city.avg_rating = round(avg)
+        city.save()
